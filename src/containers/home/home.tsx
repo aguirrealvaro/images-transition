de0e41b9ab485b47a5a3316f686d016ea8a00c16
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-07-14 02:03:21 -0300

    Init image page

--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 import { Wrapper } from "@/components";
 
 const imagesKeys = Array.from(Array(5).keys());
@@ -7,13 +8,13 @@ const Home: FunctionComponent = () => {
   return (
     <div className="flex h-full items-center">
       <Wrapper>
-        <div className="flex gap-4">
+        <div className="flex gap-2">
           {imagesKeys.map((key) => {
             const imageKey = key + 1;
             return (
-              <div key={imageKey} className="flex-1   ">
+              <Link to={`${imageKey}`} key={imageKey} className="flex-1">
                 <img src={`/${imageKey}.jpg`} alt="imagen" className="rounded-sm" />
-              </div>
+              </Link>
             );
           })}
         </div>
--- src/containers/image-page/image-page.tsx
+++ src/containers/image-page/image-page.tsx
@@ -1,7 +1,28 @@
 import { FunctionComponent } from "react";
+import { useParams } from "react-router-dom";
+import { Wrapper } from "@/components";
 
 const ImagePage: FunctionComponent = () => {
-  return <div>Image page</div>;
+  const { image } = useParams<{ image: string }>();
+
+  return (
+    <div className="flex h-full items-center">
+      <Wrapper>
+        <div className="flex gap-8">
+          <div className="w-96">
+            <img src={`/${image}.jpg`} alt="imagen" className="rounded-sm" />
+          </div>
+          <div className="flex flex-1 items-center">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod reiciendis
+              rerum, alias voluptas distinctio quibusdam dicta, accusamus aut vel provident,
+              magnam animi molestiae saepe at a repellendus porro adipisci.
+            </p>
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  );
 };
 
 export { ImagePage };
